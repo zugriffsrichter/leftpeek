cfc05f8c2fdf9bdf67459cffcaf2c4c494498e6b
ᨒ <zugriffsrichter@gmail.com>
2025-09-29 01:06:11 +0200

    Update bind.lua

--- bind.lua
+++ bind.lua
@@ -1,7 +1,7 @@
 zugriffsrichter = {
     bind = {
         command = "zugriffsrichter_leftpeak",
-        label = "Left Peek", -- name that you got in ur gta keybinds thing
-        key = "F" -- keybind you can change if you want
+        label = "Left Peek",
+        key = "G" -- change this, to add in your own keybind
     }
 }
