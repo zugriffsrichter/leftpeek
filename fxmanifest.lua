a9df38a0490ce98a9daf0fb73a0b28cb5469ed2c
ᨒ <zugriffsrichter@gmail.com>
2025-09-29 01:04:20 +0200

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'zugriffsrichter'
+description 'Left Peek Script'
+version '1.0.0'
+
+client_scripts {
+    'bind.lua',
+    'main.lua'
+}
+
+-- like it? contact me for custom, paid stuff
+
+-- github: https://github.com/zugriffsrichter
+-- discord: zugriffsrichter
+-- gmail: zugriffsrichter@gmail.com
